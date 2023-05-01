3e415ae2cf1c1d13367ed28b3661885acae35291
aftadizca <aftadizca@gmail.com>
2023-05-01 11:09:38 +0700

    ok

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 	/** @type {import('./$types').PageData} */
 	export let data;
-	console.log(data);
-	window.location.href = data.dest;
+	onMount(() => goto(data.dest, { replaceState: true }));
 </script>
