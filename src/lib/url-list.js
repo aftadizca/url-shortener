543d4b2fbfd5e7a19554a7f7687f54aacc2fbd13
aftadizca <aftadizca@gmail.com>
2023-05-01 10:50:48 +0700

    ok

---
+++ src/lib/url-list.js
@@ -0,0 +1,6 @@
+export const urls = [
+    {
+        source: 'clash',
+        destination: 'https://gist.githubusercontent.com/aftadizca/02fef7acae451b01db85ddbb8f35be08/raw/sg6_naravpn.yaml',
+    },
+]
