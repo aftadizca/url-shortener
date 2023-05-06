47b25c0e1616c7dd15fe93e7a839eeb7d0ec24e8
aftadizca <aftadizca@gmail.com>
2023-05-07 02:43:47 +0700

    ok

--- src/routes/[slug]/+page.server.js
+++ src/routes/[slug]/+page.server.js
@@ -4,6 +4,7 @@ import { urls } from '../../lib/url-list';
 
 // /** @type {import('./$types').PageLoad} */
 export function load(x) {
+    console.log(x)
     const [r] = urls.filter(item => item.source === x.params.slug)
 
     // console.log(params)
