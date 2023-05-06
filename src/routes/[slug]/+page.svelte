bd133f63f4b7ff243f5eedf90fa4f1c6dc9de010
aftadizca <aftadizca@gmail.com>
2023-05-07 03:47:31 +0700

    ok

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -4,7 +4,7 @@
 
 	/** @type {import('./$types').PageData} */
 	export let data;
-	console.log(data);
+	// console.log(data);
 	redirect(302, data.r);
 	// we can access `data.posts` because it's returned from
 	// the parent layout `load` function
