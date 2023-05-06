5a391830811f83ac697b2b203656b2d721bb12b4
aftadizca <aftadizca@gmail.com>
2023-05-07 03:22:18 +0700

    ok

--- src/routes/[slug]/+page.server.js
+++ src/routes/[slug]/+page.server.js
@@ -5,19 +5,19 @@ import { urls } from '../../lib/url-list';
 // /** @type {import('./$types').PageLoad} */
 export function load(x) {
     // console.log(x)
-    const [r] = urls.filter(item => item.source === x.params.slug)
+    // const [r] = urls.filter(item => item.source === x.params.slug)
 
-    const test = x.platform.env.URL.get("clash");
+    const test = x.platform.env.URL.get(x.params.slug);
 
 
 
     // console.log(params)
     // console.log("assasas", r)
-    if (r) {
-        return { r: test }
-        // throw redirect(308, r.destination)
+    if (test) {
+        // return { r: test }
+        throw redirect(308, test)
     }
-    return { r: test }
+    // return { r: test }
 
     // throw error(404, 'Not found');
 }
