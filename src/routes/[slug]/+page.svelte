238caa1d27f715c4ea8e621d65008e03f7b83a70
aftadizca <aftadizca@gmail.com>
2023-05-07 12:13:03 +0700

    asas

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,13 +1,15 @@
 <script>
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
-	// import { browser } from '$app/environment';
+	import { browser } from '$app/environment';
 	import { redirect } from '@sveltejs/kit';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
 	console.log(JSON.stringify(data));
-	window.location = data.r;
+	if (browser) {
+		window.location = data.r;
+	}
 	// redirect(302, data.r);
 	// we can access `data.posts` because it's returned from
 	// the parent layout `load` function
